a0d6757cb23b39850d5cb98653ffbd3913966524
Marat Sungatullin <mrsung82@gmail.com>
2025-01-11 16:33:16 +0300

    adding .h and .cpp blank files

---
+++ sources/zvd/base/zvdheaderblank.h
@@ -0,0 +1,38 @@
+/*
+******************************************************************************
+Permanent info
+--------------
+File:               zvdheaderblank.h
+Original author:    Marat Sungatullin
+Purpose:
+    C/C++ header file to start from
+Created:            jan 11, 2012
+License:
+
+    Copyright 2012-2025 Marat Sungatullin
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
+Additional info
+----------------
+Modification authors:
+
+
+******************************************************************************
+*/
+
+#ifndef HEADER_FILE_NAME_GUARD
+#define HEADER_FILE_NAME_GUARD
+
+#endif // HEADER_FILE_NAME_GUARD
